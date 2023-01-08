5606a75f21723a95431886022b0aa1f6f4117b0a
mikael.code.dev <mikael.code.dev@outlook.com>
2023-01-08 18:56:54 +0100

    Add project files.

---
+++ Identity_test_01/Data/ApplicationDbContext.cs
@@ -0,0 +1,109 @@
+﻿using Identity_test_01.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity_test_01.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            string adminRoleId = new Guid().ToString();
+            string managerRoleId = new Guid().ToString();
+            string customerRoleId = new Guid().ToString();
+            string adminUserId = new Guid().ToString();
+            string managerUserId = new Guid().ToString();
+            string customerUserId = new Guid().ToString();
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = adminRoleId,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = managerRoleId,
+                    Name = "Manager",
+                    NormalizedName = "MANAGER"
+                },
+                new IdentityRole
+                {
+                    Id = customerRoleId,
+                    Name = "Customer",
+                    NormalizedName = "CUSTOMER"
+                });
+
+            PasswordHasher<ApplicationUser> hasher = new PasswordHasher<ApplicationUser>();
+
+            modelBuilder.Entity<ApplicationUser>().HasData(
+                new ApplicationUser
+                {
+                    Id = adminUserId,
+                    Email = "admin@admin.com",
+                    NormalizedEmail = "ADMIN@ADMIN.COM",
+                    UserName = "admin@admin.com",
+                    NormalizedUserName = "ADMIN@ADMIN.COM",
+                    FirstName = "Admin",
+                    LastName = "Adminsson",
+                    PasswordHash = hasher.HashPassword(null, "1234")
+                },
+                new ApplicationUser
+                {
+                    Id = managerUserId,
+                    Email = "manager@manager.com",
+                    NormalizedEmail = "MANAGER@MANAGER.COM",
+                    UserName = "manager@manager.com",
+                    NormalizedUserName = "MANAGER@MANAGER.COM",
+                    FirstName = "Manager",
+                    LastName = "Managersson",
+                    PasswordHash = hasher.HashPassword(null, "1234")
+                },
+                new ApplicationUser
+                {
+                    Id = customerRoleId,
+                    Email = "customer@customer.com",
+                    NormalizedEmail = "CUSTOMER@CUSTOMER.COM",
+                    UserName = "customer@customer.com",
+                    NormalizedUserName = "CUSTOMER@CUSTOMER.COM",
+                    FirstName = "Customer",
+                    LastName = "Customersson",
+                    PasswordHash = hasher.HashPassword(null, "1234")
+                });
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRoleId,
+                    UserId = adminUserId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = managerRoleId,
+                    UserId = managerUserId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = customerRoleId,
+                    UserId = customerUserId
+                });
+        }
+
+
+    }
+}
---
+++ Identity_test_01/Program.cs
@@ -0,0 +1,57 @@
+using Identity_test_01.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Identity_test_01.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContextConnection"));
+});
+
+
+
+
+//var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
+
+
+
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
